74be6de0e2f175e93c7429b4f245248986a29142
Shelby Falde <theseventhdegree7@gmail.com>
2025-06-06 17:47:30 -0700

    Add sound effects, fixed overlay css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,6 +15,9 @@ export default function App({ username }) {
 
   return (
     <div>
+      <div style={{ backgroundColor: "#222", padding: "12px 0" }}>
+        <header className="logo-header">IRONKEEP</header>
+      </div>
       <div style={{ padding: "8px", background: "#222", color: "#fff" }}>
         Logged in as: {username}
       </div>
