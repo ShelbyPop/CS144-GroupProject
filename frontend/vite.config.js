80f945265351ca44faf471e6799300c4450723c7
nleobandung <nathan.leobandung@gmail.com>
2025-06-10 22:32:23 -0700

    final touches

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -45,6 +45,16 @@ export default defineConfig({
       }
     }),
   ],
+  server: {
+    host: '0.0.0.0',  
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000', 
+        changeOrigin: false,
+        secure: false,
+      },
+    },
+  },
   preview: {
     allowedHosts: ['team24.cs144.org'],
   },
